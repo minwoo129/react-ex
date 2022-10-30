d2d278af3613d58bfe4a306506c6fd49694e94bf
minwoo129 <key0129mw@naver.com>
2022-10-30 17:42:05 +0900

    blog-frontend: router 연결

--- blog-frontend/src/App.tsx
+++ blog-frontend/src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+import PostPage from './pages/PostPage';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route element={<PostListPage />} path="/*" />
+      <Route element={<PostListPage />} path="/@:username/*" />
+      <Route element={<LoginPage />} path="/login/*" />
+      <Route element={<RegisterPage />} path="/register/*" />
+      <Route element={<WritePage />} path="/write/*" />
+      <Route element={<PostPage />} path="/@:username/:postId/*" />
+    </Routes>
   );
-}
+};
 
 export default App;
